69604fa8e0dac7de1262c6d9a5c059faf6b9c7b2
nicoverdin <nverdind@gmail.com>
2025-12-26 16:59:28 +0100

    feat: fix catalog bug

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="h-[100dvh] w-full flex flex-col overflow-hidden bg-bg-main relative">
+  <div class="min-h-screen flex flex-col">
+    <Navbar v-if="route.name !== 'Login'" />
     
-    <Navbar v-if="route.name !== 'Login'" class="flex-none z-50" />
-    
-    <main class="flex-grow overflow-y-auto overflow-x-hidden scroll-smooth">
-      <div class="w-full" style="height: calc(80px + env(safe-area-inset-top));"></div>
-      
+    <main class="flex-grow">
       <router-view />
     </main>
   </div>
@@ -17,9 +14,3 @@ import { useRoute } from 'vue-router';
 
 const route = useRoute();
 </script>
-
-<style>
-main {
-  -webkit-overflow-scrolling: touch;
-}
-</style>
